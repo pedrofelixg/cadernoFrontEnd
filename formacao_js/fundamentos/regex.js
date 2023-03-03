d2bb60668813c04e9ac623d58a5d45822db53a79
pedrofelixg <pedro.viniciusacm@gmail.com>
2023-03-02 21:25:44 -0300

    add validação de senha

--- formacao_js/fundamentos/regex.js
+++ formacao_js/fundamentos/regex.js
@@ -11,3 +11,15 @@ function validacaoEmail(email) {
 validacaoEmail("teste@gmail.com");
 
 // estrutura de validação visto em https://horadecodar.com.br/2020/09/13/como-validar-email-com-javascript/
+
+function validacaoSenha (senha) {
+    const regexSenha = RegExp(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#])[0-9a-zA-Z$*&@#]{8,}$/);
+
+    if(regexSenha.test(senha)){
+        console.log("a senha é forte");
+    } else {
+        console.log("a senha é fraca")
+    }
+}
+
+validacaoSenha("Ventilador@16");
