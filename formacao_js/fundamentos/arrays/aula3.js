30c88768cd0742efaf21b87c65c4af83831baa71
pedrofelixg <pedro.viniciusacm@gmail.com>
2023-03-21 21:01:22 -0300

    segunda parte da aula 3

--- formacao_js/fundamentos/arrays/aula3.js
+++ formacao_js/fundamentos/arrays/aula3.js
@@ -48,3 +48,29 @@ const aquario = ["baleia", "lula", "golfinho", "tubarao"];
 aquario.splice(1, 0, "peixinho");
 aquario.splice(3, 2, "peixe azul");
 console.log(aquario);
+
+// juntando arrays
+
+const salaJS = ["Everaldo", "Matias", "Ilía", "Morena", "Laura"];
+const salaPY = ["Nikolai", "Thomas", "Albert", "Milan", "Hannah"];
+
+const salasJuntas = salaJS.concat(salaPY);
+console.log(salasJuntas);
+
+// não modifica o array
+
+// arrays 2D
+const estudantes = [
+    "João", "Juliana", "Ana", "Caio"
+];
+
+const mediasEstudantes = [
+    10, 8, 7.5, 9
+];
+
+const estudantesMedias = [estudantes, mediasEstudantes];
+console.log(estudantesMedias); // acessa os valores desse array é preciso passar dois parametros em colchetes
+
+console.log(
+    `o estudante ${estudantesMedias[0][3]} tem média ${estudantesMedias[1][3]} nesta escola.`
+);
