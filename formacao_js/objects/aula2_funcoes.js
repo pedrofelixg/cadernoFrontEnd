af3668f3a18bd31602cea632213660ed39072781
pedrofelixg <pedro.viniciusacm@gmail.com>
2023-05-11 20:31:21 -0300

    passando funções como parâmetros em um objeto

---
+++ formacao_js/objects/aula2_funcoes.js
@@ -0,0 +1,22 @@
+// implementação de funções nas propriedades de um objeto.
+const cliente = {
+    nome: "Paulo César Nogueira",
+    idade: 36,
+    email: "pcnogueira@gmail.com",
+    telefones: ["81 996374512", "81 997212515"],
+    saldo: 200,
+    deposito: function(deposito){
+        console.log(deposito + this.saldo);
+        return this.saldo;
+    },
+    efetuaPagamento: function (valor) {
+        if(valor > this.saldo) {
+            console.log("Compra não autorizada, saldo insuficiente")
+        } else {
+            this.saldo -= valor;
+            console.log(`Pagamento realizado. Novo saldo: ${this.saldo}`)
+        }
+    },
+};
+
+cliente.efetuaPagamento(10);
