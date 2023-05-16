b6e1eec1a7b9ada0b78f2682130b08c4e14ccf15
pedrofelixg <pedro.viniciusacm@gmail.com>
2023-05-15 22:20:25 -0300

    utilizando métodos de array em objetos para encontrar, filtrar e ordenar as propriedades

--- formacao_js/objects/aula5_testeClientes.js
+++ formacao_js/objects/aula5_testeClientes.js
@@ -1,2 +1,48 @@
 const clientes = require("./aula5_clientes.json");
 console.log(typeof clientes);
+
+// método for...in
+// for (const dados in clientes) {
+//     if (Object.hasOwnProperty.call(clientes, dados)) {
+//         const dado = clientes[dados];
+//         console.log(dado);
+//     }
+// };
+
+// encontrar objeto.
+function encontrar(objeto, chave, valor) {
+    return objeto.find((item) => item[chave].includes(valor));
+}
+
+const encontrado = encontrar(clientes, "nome", "Kirby");
+console.log(encontrado);
+
+// filtrando objetos
+
+function filterNoComplements(lista) {
+    return lista.filter((cliente) => {
+        return (cliente.endereco.apartamento &&
+            !cliente.endereco.hasOwnProperty("complemento"));
+    });
+}
+
+const clientesFiltrados = filterNoComplements(clientes);
+console.log(clientesFiltrados);
+
+//ordenando
+function ordenamento(lista, propriedade) {
+    const resultado = lista.sort((a, b) => {
+        if(a[propriedade] < b[propriedade]) {
+            return -1;
+        }
+        if(a[propriedade] > b[propriedade]) {
+            return 1;
+        }
+        return 0;
+    })
+
+    return resultado;
+}
+
+const ordenadoPorNome = ordenamento(clientes, "nome");
+console.log(ordenadoPorNome);
